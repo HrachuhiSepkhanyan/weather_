c8863f57ddc627952cf4f7eb1459f332d7e1e99b
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2022-07-01 14:44:29 +0400

    remove citySclices from redux store

--- src/components/favoriteCity/CitiesList.tsx
+++ src/components/favoriteCity/CitiesList.tsx
@@ -7,7 +7,6 @@ import { Style } from "./CitiesList.styled";
 type CityListProps = {
   cities: ICity[];
 };
-
 const t = new Date();
 const dd = String(t.getDate()).padStart(2, "0");
 const mm = String(t.getMonth() + 1).padStart(2, "0");
