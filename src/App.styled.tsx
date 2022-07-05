9e08ba430bb71120a60a647bb27de8cc895ce378
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2022-07-05 14:48:54 +0400

    added App Container:

---
+++ src/App.styled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  text-align: center;
+
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+`;
