dd0b5358fdfcb5ae0df8068f9dae3bce54df1bde
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2022-07-21 14:47:48 +0400

    feat:loading

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { Style } from "./Home.style";
 import Weather from "../../weather/Weather";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../redux/store";
 
 const Home: React.FunctionComponent = () => {
-  const showLoading = useSelector(
-    (state: RootState) => state.weatherData.showLoading
-  );
-  console.log(showLoading);
   return (
     <Style.Content>
       <Weather />
